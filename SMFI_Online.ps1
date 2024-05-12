596a28e7b0c2cda2ed268d67e962becf1e0b7f0e
SMControl <143865768+SMControl@users.noreply.github.com>
2024-05-12 08:54:31 +0100

    Update SMFI_Online.ps1
    made permissions part quiet

--- SMFI_Online.ps1
+++ SMFI_Online.ps1
@@ -31,7 +31,7 @@ if (!(Test-Path "C:\Program Files (x86)\Firebird")) {
     ################################
     # Part 5 - Adjusting permissions
     ################################
-    icacls "C:\Program Files (x86)\Firebird" /grant "*S-1-1-0:(OI)(CI)F" /T /C
+    icacls "C:\Program Files (x86)\Firebird" /grant "*S-1-1-0:(OI)(CI)F" /T /C | Out-Null
 
     ################################
     # Part 6 - Start Firebird service
